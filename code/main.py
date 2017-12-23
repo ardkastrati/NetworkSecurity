6d2b3c10db80458722b9f66b953165bf4c8b8c66
alexus37 <alexlelidis@gmx.de>
2017-12-24 00:10:51 +0100

    add first version of webpage with simple flask server

---
+++ code/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, make_response
+app = Flask(__name__)
+
+currentValidCookie = "123"
+
+'''
+    This methodes checkes the cookie 
+'''
+def checkCookie(cookie):
+    if cookie == currentValidCookie:
+        return True
+    else:
+        return False
+
+@app.route('/')
+def display():
+    response = make_response(render_template('login.html'))
+    response.set_cookie('terrorLoginCookie',value='notAuth')
+    return response
+
+@app.route('/home')
+def displayHome():
+    if 'terrorLoginCookie' in request.cookies and checkCookie(request.cookies['terrorLoginCookie']):
+        return render_template('home.html')
+    else:
+        return display()
+
+@app.route('/news')
+def displayNews():
+    if 'terrorLoginCookie' in request.cookies and checkCookie(request.cookies['terrorLoginCookie']):
+        return render_template('news.html')
+    else:
+        return display()
+
+@app.route('/chat')
+def displayChat():
+    if 'terrorLoginCookie' in request.cookies and checkCookie(request.cookies['terrorLoginCookie']):
+        return render_template('chat.html')
+    else:
+        return display()
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', debug=True, port=5000)
