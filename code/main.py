ca0493baaaafc49292e3e767f2364109b881a920
abaguas <andre.b.aguas@gmail.com>
2018-01-06 18:50:46 +0000

    creates a home page for user1 and admin; points the links in the menu to the correct pages

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, make_response, send_from_directory, session
+import json
 import random
 import os
 import threading
@@ -11,7 +12,8 @@ loggedInUserCookies = {}
 
 prng = random.Random(1337)
 
-userData = {"user1":"password1"}
+userData = {"user1": "password1"}
+
 
 """ Create a task which logs in the admin every ADMIN_PERIOD seconds """
 def adminLogin():
@@ -29,7 +31,7 @@ def adminLogin():
             token = key
             break
 
-    if token != None:
+    if token is not None:
         loggedInUserCookies[token] = None
 
     token = str(prng.getrandbits(32))
@@ -46,47 +48,41 @@ def adminLogin():
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
+
 @app.route('/')
 def display():
     global loggedInUserCookies
 
-    # print(loggedInUserCookies)
+    with open('db.json', 'r') as file:
+        db = json.load(file)
 
     try:
-        # If there is no login cookie, show login page
+        # send the user to its home
         #
-        if not "netsecLoginCookie" in request.cookies:
-            return make_response(render_template('login.html'))
-        # If we have an admin, send him to admin home
+        user = loggedInUserCookies[request.cookies.get('netsecLoginCookie')]
+        return make_response(render_template('home.html', profile=db['profiles'][user]))
+        # If there is no login cookie or it is not in the hashtable, send user back to login page
         #
-        elif loggedInUserCookies[request.cookies['netsecLoginCookie']] == 'admin':
-            return make_response(render_template('adminHome.html'))
-        # If we have a logged in user, send him to user home
-        #
-        elif loggedInUserCookies[request.cookies['netsecLoginCookie']] != None:
-            return make_response(render_template('home.html'))
-        # If the login token isn't in a hashtable, send user back to login page
-        #
-        else:
-            return make_response(render_template('login.html'))
     except KeyError:
         return make_response(render_template('login.html'))
 
 
-
-@app.route('/login', methods=['GET','POST'])
-def loginForm():
+@app.route('/login', methods=['GET', 'POST'])
+def login_form():
     global loggedInUserCookies
     global prng
 
     data = request.form
     value = str(prng.getrandbits(32))
 
+    with open('db.json', 'r') as file:
+        db = json.load(file)
+
     # This can be used to test admin pages. In the final version, admin prompt should be removed
     #
     if data['user'] == 'admin' and data['password'] == 'verylongandsecretpassword':
-        response = make_response(render_template('adminHome.html'))     
-        response.set_cookie('netsecLoginCookie',value)
+        response = make_response(render_template('home.html', profile=db['profiles']['admin']))
+        response.set_cookie('netsecLoginCookie', value)
         loggedInUserCookies[value] = 'admin'
         session['username'] = 'admin'
         return response
@@ -94,19 +90,24 @@ def loginForm():
     # Log in user, and send him his cookie :)
     #
     elif userData[data['user']] == data['password']:
-        response = make_response(render_template('home.html'))
+        response = make_response(render_template('home.html', profile=db['profiles'][data['user']]))
         response.set_cookie('netsecLoginCookie', value)
         loggedInUserCookies[value] = data['user']
         session['username'] = data['user']
         return response
+    # If the login is fails the login page is sent again
+    #
+    else:
+        return make_response(render_template('login.html'))
 
-@app.route('/logout', methods=['GET','POST'])
-def logOut():
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
 
     # Redirect user to login, and expire his login cookie
     #
     response = make_response(render_template('login.html'))
-    response.set_cookie('netSecLoginCookie','', expires=0)
+    response.set_cookie('netSecLoginCookie', '', expires=0)
     
     # Find the token with the username, and delete it
     #
@@ -114,18 +115,16 @@ def logOut():
 
     try:
         username = session['username']
-        
-    
+
         for key, value in loggedInUserCookies.items():
             if value == username:
                 token = key
                 break
     
-    
     except Exception:
         return response
 
-    if token == None:
+    if token is None:
         return response
 
     loggedInUserCookies[token] = None
@@ -133,10 +132,86 @@ def logOut():
     return response
 
 
-if __name__=='__main__':
+@app.route('/news', methods=['GET', 'POST'])
+def news():
+    with open('db.json', 'r') as file:
+        db = json.load(file)
+    try:
+        user = loggedInUserCookies[request.cookies.get('netsecLoginCookie')]
+        return make_response(render_template('news.html', news=db['profiles'][user]['news']))
+    except KeyError:
+        return make_response(render_template('login.html'))
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    with open('db.json', 'r') as file:
+        db = json.load(file)
+    try:
+        user = loggedInUserCookies[request.cookies.get('netsecLoginCookie')]
+        return make_response(render_template('chat.html', chat=db['profiles'][user]['chat'],
+                                             profile_pic=db['profiles'][user]['image']))
+    except KeyError:
+        return make_response(render_template('login.html'))
+
+
+""" Create a json dump with the user profile's data """
+def populate_db():
+    data = {'profiles': {'admin': {'name': 'Johnny Appleseed',
+                                   'city': 'San Francisco, CA',
+                                   'reputation': 185,
+                                   'kills_title': 'Kills',
+                                   'kills': 344,
+                                   'days out': 127,
+                                   'about me': 'Eniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                   'image': 'static/terrorist2.jpg',
+                                   'friends': 'Your friends - 106 total',
+                                   'friend names': ['Osama', 'Imad', 'Hasan', 'Ali', 'Abdul', 'Mohamed'],
+                                   'chat': {'messages': [["Hello. How are you today?", "Hey! I'm fine. Thanks for asking!"],
+                                                         ["Sweet! So, are you ready for your mission?", "Yes bro I am so ready! 72 Virgins cant wait."],
+                                                         ["All right, you know what to do?", "Yes, I just need the coordinates!"],
+                                                         ],
+                                            'image message': ['static/image.jpg', 'All right! See you on ther other side'],
+                                            'image recipient': 'static/terrorist1.jpg'
+                                            },
+                                   'news': {'cover': 'static/cover.jpg', 'one third': 'static/rifle.jpg',
+                                            'two thirds': 'static/grenade.jpg', 'three thirds': 'static/knife.jpg',
+                                            'final': 'static/explosion.jpg'}
+                                   },
+                         'user1': {'name': 'Philip Dickens',
+                                   'city': 'Boston, MA',
+                                   'reputation': 0,
+                                   'kills_title': 'Followers',
+                                   'kills': 12,
+                                   'days out': 0,
+                                   'about me': 'Eniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                   'image': 'static/user_profile_pic.jpg',
+                                   'friends': 'Your friends - 59 total',
+                                   'friend names': ['Jerry K.', 'Katie F.', 'Ash K.', 'Sponge B.', 'Frank', 'Alexa S.'],
+                                   'chat': {'messages': [["Hello Mom. How are you today?", "Hello darling! I'm fine. How are you?"],
+                                                         ["I am good too. Can you buy some ice cream? ", "Of course son!"],
+                                                         ["Thank you mom! You are the best!", "I know. Don't forget to tidy your room before your dad gets home"],
+                                                         ["No worries, I already tidied it", "Great! A big kiss!"],
+                                                         ["Love you! Bye", "Bye bye my baby <3"],
+                                                         ],
+                                            'image recipient': 'static/mom.jpg'
+                                            },
+                                   'news': {'cover': 'static/friendly_cover.jpg', 'one third': 'static/ship.jpg',
+                                            'two thirds': 'static/pengu.jpg', 'three thirds': 'static/trump.jpg',
+                                            'final': 'static/countries.jpg'}
+                                   }
+                         }
+            }
+
+    with open('db.json', 'w') as file:
+        json.dump(data, file)
+
+
+if __name__ == '__main__':
 
     # Schedule admin login code
     #
+    populate_db()
     adminLogin()
 
     # Generate application key so we can use flask sessions
